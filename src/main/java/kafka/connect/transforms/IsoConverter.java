edc6e8c387043839094759cb4e98c1043f42fdf9
ddolboghi <rhdfyd128@gmail.com>
2024-12-11 10:35:10 +0900

    feat: 컴포넌트 버전 반환 메서드 구현

--- src/main/java/kafka/connect/transforms/IsoConverter.java
+++ src/main/java/kafka/connect/transforms/IsoConverter.java
@@ -1,5 +1,36 @@
 package kafka.connect.transforms;
 
-public class IsoConverter {
+import java.util.Map;
+import org.apache.kafka.common.config.ConfigDef;
+import org.apache.kafka.common.utils.AppInfoParser;
+import org.apache.kafka.connect.components.Versioned;
+import org.apache.kafka.connect.connector.ConnectRecord;
+import org.apache.kafka.connect.transforms.Transformation;
 
+public class IsoConverter<R extends ConnectRecord<R>> implements Transformation<R>, Versioned {
+
+    @Override
+    public String version() {
+        return AppInfoParser.getVersion();
+    }
+
+    @Override
+    public R apply(R record) {
+        return null;
+    }
+
+    @Override
+    public ConfigDef config() {
+        return null;
+    }
+
+    @Override
+    public void close() {
+
+    }
+
+    @Override
+    public void configure(Map<String, ?> map) {
+
+    }
 }
